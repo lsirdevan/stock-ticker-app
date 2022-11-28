8732262c73dac7ca6abeeed56377be5f8e61dcb0
Greg Mundy <greg@grifin.com>
2022-11-28 12:40:36 -0500

    feat: add dotenv and env local configuration

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { ALPHA_VANTAGE_API_KEY } from '@env';
+
+const findStockName = async () => {
+
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>{ALPHA_VANTAGE_API_KEY}</Text>
       <StatusBar style="auto" />
     </View>
   );
